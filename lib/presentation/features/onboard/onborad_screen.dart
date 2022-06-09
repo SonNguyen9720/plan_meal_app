c5fa7478a89b0c5983687dd99b317d8519840ac5
Son Nguyen <darkeyes123qn@gmail.com>
2022-06-10 03:14:01 +0700

    Add carousel_item

--- lib/presentation/features/onboard/onborad_screen.dart
+++ lib/presentation/features/onboard/onborad_screen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:plan_meal_app/config/theme.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:plan_meal_app/data/model/carousel_item.dart';
 
 class OnboardScreen extends StatelessWidget {
-  const OnboardScreen({Key? key}) : super(key: key);
+  OnboardScreen({Key? key}) : super(key: key);
+  final List<CarouselItem> carouselItemList = [
+    const CarouselItem(
+        image: "assets/intro/onboard_1.png",
+        title: "Eat Healthy",
+        description:
+            "Maintaining good health should be the primary focus of everyone."),
+    const CarouselItem(
+        image: "assets/intro/onboard_2.png",
+        title: "Healthy Recipes",
+        description:
+            "Browse thousands of healthy recipes from all over the world."),
+    const CarouselItem(
+        image: "assets/intro/onboard_3.png",
+        title: "Track Your Health",
+        description: "With amazing inbuilt tools you can track your progress.")
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +36,15 @@ class OnboardScreen extends StatelessWidget {
                   fontWeight: FontWeight.w700),
             ),
           ),
+          CarouselSlider.builder(
+              itemCount: carouselItemList.length,
+              itemBuilder: (context, index, realIndex) {
+                final carouselItem = carouselItemList[index];
+                return buildCarouselItem(carouselItem);
+              },
+              options: CarouselOptions(
+                height: 400,
+              )),
           Center(
             child: SizedBox(
               height: 72,
@@ -62,6 +89,51 @@ class OnboardScreen extends StatelessWidget {
                 onTap: () {},
               )
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  // List buildCarouselItemList() {
+  //   List carouselItemList = [];
+  //   for (int i = 0; i < imageList.length; i++) {
+  //     carouselItemList.add(
+  //         buildCarouselItem(imageList[i], titleList[i], descriptionList[i]));
+  //   }
+  //   return carouselItemList;
+  // }
+
+  Widget buildCarouselItem(CarouselItem carouselItem) {
+    return SizedBox(
+      width: 282,
+      child: Column(
+        children: [
+          Image.asset(
+            carouselItem.image,
+            height: 282,
+            width: 282,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                carouselItem.title,
+                style:
+                    const TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 282,
+            child: Center(
+              child: Text(
+                carouselItem.description,
+                style: const TextStyle(fontSize: 17),
+                textAlign: TextAlign.center,
+              ),
+            ),
           )
         ],
       ),
