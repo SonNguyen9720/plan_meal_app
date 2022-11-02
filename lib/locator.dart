f4962d779d00a8b823d92dcf3927614d3dc1a874
Son Nguyen <darkeyes123qn@gmail.com>
2022-11-03 04:16:15 +0700

    Refactor code

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:plan_meal_app/data/repositories/abstract/group_repository.dart';
 import 'package:plan_meal_app/data/repositories/abstract/user_repository.dart';
-import 'package:plan_meal_app/data/repositories/remote_repositories/group_repository_impl.dart';
+import 'package:plan_meal_app/data/repositories/group_repository_impl.dart';
 import 'package:plan_meal_app/data/repositories/remote_repositories/repositories/group_repository_remote.dart';
 import 'package:plan_meal_app/data/repositories/remote_repositories/repositories/user_repository_remote.dart';
 import 'package:plan_meal_app/data/repositories/user_repository_impl.dart';
