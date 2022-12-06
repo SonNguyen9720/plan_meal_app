2dd9ff65565c4b3d90ac97cc8b6ccde90b514d37
Son Nguyen <darkeyes123qn@gmail.com>
2022-12-06 23:06:45 +0700

    Add recipe

--- lib/presentation/features/food/food_detail/food_detail_screen.dart
+++ lib/presentation/features/food/food_detail/food_detail_screen.dart
@@ -24,143 +24,199 @@ class FoodDetailScreen extends StatelessWidget {
                 SliverAppBar(
                   expandedHeight: 160,
                   flexibleSpace: FlexibleSpaceBar(
-                    background: state.foodDetailEntity.imageUrl.isNotEmpty ? Image.network(
-                      state.foodDetailEntity.imageUrl,
-                      fit: BoxFit.cover,
-                    ) : Container(color: AppColors.grey100,),
+                    background: state.foodDetailEntity.imageUrl.isNotEmpty
+                        ? Image.network(
+                            state.foodDetailEntity.imageUrl,
+                            fit: BoxFit.cover,
+                          )
+                        : Container(
+                            color: AppColors.grey100,
+                          ),
                   ),
                 ),
               ],
-              body: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      child: Text(
-                        state.foodDetailEntity.name,
-                        style: const TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
+              body: SingleChildScrollView(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        child: Text(
+                          state.foodDetailEntity.name,
+                          style: const TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 16),
-                          padding:
-                          const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                          decoration: const BoxDecoration(
-                              color: AppColors.orangeLight,
-                              borderRadius: BorderRadius.all(Radius.circular(6))),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Icon(
-                                Icons.local_fire_department,
-                                color: AppColors.red,
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Text("${state.foodDetailEntity.calories} kcal",
-                                  style: const TextStyle(
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 16),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 16),
+                            decoration: const BoxDecoration(
+                                color: AppColors.orangeLight,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(6))),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.local_fire_department,
+                                  color: AppColors.red,
+                                ),
+                                const SizedBox(
+                                  width: 8,
+                                ),
+                                Text("${state.foodDetailEntity.calories} kcal",
+                                    style: const TextStyle(
+                                      color: AppColors.white,
+                                      fontSize: 20,
+                                    )),
+                              ],
+                            ),
+                          ),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 16),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 16),
+                              decoration: const BoxDecoration(
+                                  color: AppColors.green,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(6))),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Icon(
+                                    Icons.access_time_filled,
                                     color: AppColors.white,
-                                    fontSize: 20,
-                                  )),
-                            ],
-                          ),
+                                  ),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    "${state.foodDetailEntity.cookingTime} min",
+                                    style: const TextStyle(
+                                        fontSize: 20, color: AppColors.white),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 16),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 8),
+                        decoration: const BoxDecoration(
+                            color: AppColors.breakfastTag,
+                            borderRadius: BorderRadius.all(Radius.circular(6))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "${state.foodDetailEntity.protein} g",
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  "Proteins",
+                                  style: TextStyle(
+                                    color: AppColors.gray,
+                                    fontSize: 16,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 40,
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "${state.foodDetailEntity.carb} g",
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  "Carbs",
+                                  style: TextStyle(
+                                    color: AppColors.gray,
+                                    fontSize: 16,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 40,
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "${state.foodDetailEntity.fat} g",
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Text(
+                                  "Fats",
+                                  style: TextStyle(
+                                    color: AppColors.gray,
+                                    fontSize: 16,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
                         ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Text("${state.foodDetailEntity.cookingTime} min cooking", style: const TextStyle(fontSize: 20),)),
-                      ],
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 16),
-                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                      decoration: const BoxDecoration(
-                          color: AppColors.breakfastTag,
-                          borderRadius: BorderRadius.all(Radius.circular(6))),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                "${state.foodDetailEntity.protein} g",
-                                style: const TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Text(
-                                "Proteins",
-                                style: TextStyle(
-                                  color: AppColors.gray,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 40,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                "${state.foodDetailEntity.carb} g",
-                                style: const TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Text(
-                                "Carbs",
-                                style: TextStyle(
-                                  color: AppColors.gray,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 40,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                "${state.foodDetailEntity.fat} g",
-                                style: const TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Text(
-                                "Fats",
-                                style: TextStyle(
-                                  color: AppColors.gray,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
                       ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          const Text("Description", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                          Text("${state.foodDetailEntity.description}"),
-                        ],
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            const Text(
+                              "Description",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              state.foodDetailEntity.description,
+                              style: const TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            const Text(
+                              "Recipe",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              state.foodDetailEntity.recipe,
+                              style: const TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
