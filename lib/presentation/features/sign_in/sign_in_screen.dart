c4311ce518919ab2323362f064f447d3a7933350
Son Nguyen <darkeyes123qn@gmail.com>
2022-12-07 09:30:26 +0700

    Update sign in screen

--- lib/presentation/features/sign_in/sign_in_screen.dart
+++ lib/presentation/features/sign_in/sign_in_screen.dart
@@ -24,77 +24,87 @@ class _SignInScreenState extends State<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: AppColors.white,
-        body: BlocConsumer<SignInBloc, SignInState>(
-          listener: (context, signInState) async {
-            if (signInState is SignInFinishedState) {
-              print("Log in success");
-              await EasyLoading.dismiss();
-              Navigator.of(context).pushReplacementNamed(PlanMealRoutes.home);
-            }
-            if (signInState is SignInProcessingState) {
-              EasyLoading.show(
-                status: "Loading ...",
-                maskType: EasyLoadingMaskType.black,
-              );
-            }
-            if (signInState is SignInErrorState) {
-              await EasyLoading.dismiss();
-              showDialog(context: context, builder: (context) => AlertDialog(title: Text(signInState.error),));
-            }
-          },
-          builder: (context, signInState) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Flexible(
-                  flex: 3,
-                  child: Text(
-                    "Sign in",
-                    style: GoogleFonts.inter(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 40,
-                      height: 1.5,
+    return SafeArea(
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: AppColors.white,
+          body: BlocConsumer<SignInBloc, SignInState>(
+            listener: (context, signInState) async {
+              if (signInState is SignInFinishedState) {
+                print("Log in success");
+                await EasyLoading.dismiss();
+                Navigator.of(context).pushReplacementNamed(PlanMealRoutes.home);
+              }
+              if (signInState is SignInProcessingState) {
+                EasyLoading.show(
+                  status: "Loading ...",
+                  maskType: EasyLoadingMaskType.black,
+                );
+              }
+              if (signInState is SignInErrorState) {
+                await EasyLoading.dismiss();
+                showDialog(context: context, builder: (context) => AlertDialog(title: Text(signInState.error),));
+              }
+            },
+            builder: (context, signInState) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    flex: 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Sign in",
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 40,
+                            height: 1.5,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(vertical: 16),
+                            child: Image.asset("assets/app_logo.png", height: 128,width: 128,)),
+                      ],
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 7,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InputField(
-                        key: emailKey,
-                        controller: emailController,
-                        icon: Icons.email,
-                        hint: "Input your email",
-                        validator: Validator.checkEmail,
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      InputField(
-                        key: passwordKey,
-                        controller: passwordController,
-                        icon: Icons.password,
-                        hint: "Input your password",
-                        isPassword: true,
-                        validator: Validator.checkPasswordCorrect,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 40),
-                        child: NavigateButton(
-                            text: "Sign in", callbackFunc: _validateAndSend),
-                      )
-                    ],
+                  Flexible(
+                    flex: 6,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        InputField(
+                          key: emailKey,
+                          controller: emailController,
+                          icon: Icons.email,
+                          hint: "Input your email",
+                          validator: Validator.checkEmail,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        InputField(
+                          key: passwordKey,
+                          controller: passwordController,
+                          icon: Icons.password,
+                          hint: "Input your password",
+                          isPassword: true,
+                          validator: Validator.checkPasswordCorrect,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 40),
+                          child: NavigateButton(
+                              text: "Sign in", callbackFunc: _validateAndSend),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            );
-          },
-        ));
+                ],
+              );
+            },
+          )),
+    );
   }
 
   void _validateAndSend() {
