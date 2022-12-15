aea430351b2b9dfcae9af6cf3fe4960b5f3b3851
Son Nguyen <darkeyes123qn@gmail.com>
2022-12-15 17:05:43 +0700

    Update avatar

--- lib/presentation/features/profile/avatar_bloc/bloc/avatar_bloc.dart
+++ lib/presentation/features/profile/avatar_bloc/bloc/avatar_bloc.dart
@@ -1,13 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:plan_meal_app/data/repositories/abstract/firebase_repository.dart';
+import 'package:plan_meal_app/data/repositories/abstract/user_repository.dart';
 
 part 'avatar_event.dart';
 part 'avatar_state.dart';
 
 class AvatarBloc extends Bloc<AvatarEvent, AvatarState> {
-  AvatarBloc() : super(AvatarInitial()) {
-    on<AvatarEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final FirebaseFireStoreRepository firebaseFireStoreRepository;
+  final UserRepository userRepository;
+
+  AvatarBloc(
+      {required this.firebaseFireStoreRepository, required this.userRepository})
+      : super(AvatarInitial()) {
+    on<AvatarPickFromCameraEvent>(onAvatarPickFromCameraEvent);
+  }
+
+  void onAvatarPickFromCameraEvent(
+      AvatarPickFromCameraEvent event, Emitter<AvatarState> emit) async {
+    XFile? pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    String result = '';
+    if (pickedFile != null) {
+      emit(AvatarWaiting());
+      result = await firebaseFireStoreRepository.uploadAvatar(pickedFile);
+    }
   }
 }
--- lib/presentation/features/profile/avatar_bloc/bloc/avatar_state.dart
+++ lib/presentation/features/profile/avatar_bloc/bloc/avatar_state.dart
@@ -2,9 +2,13 @@ part of 'avatar_bloc.dart';
 
 abstract class AvatarState extends Equatable {
   const AvatarState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class AvatarInitial extends AvatarState {}
+
+class AvatarWaiting extends AvatarState {}
+
+class AvatarFinished extends AvatarState {}
